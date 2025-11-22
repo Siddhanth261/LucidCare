e23f4a7354de1170ef0b49a339ce04be7bc6df08
DARSHAN GOLCHHA <darshangolchha@DARSHANs-MacBook-Air-7.local>
2025-11-22 14:49:07 -0600

    setting up docker compose

--- emotion-app/vite.config.js
+++ emotion-app/vite.config.js
@@ -5,4 +5,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    watch: {
+      usePolling: true, // Needed for Docker on some systems
+    },
+    host: true,         // CRITICAL: Allows Docker port mapping to work
+    strictPort: true,
+    port: 5173,
+  }
 })
